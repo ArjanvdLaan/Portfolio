fe0e8c0e0c46741e344ea7fb0a5437dffa2cd959
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-01-10 16:57:51 +0100

    add vite-plugin-copy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import copy from 'vite-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    copy({
+      targets: [
+        { src: '_redirects', dest: 'dist' }
+      ]
+    })
+  ],
   server: {
     proxy: {
       '/api': {
