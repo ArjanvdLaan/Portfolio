3115c4448fefba1035b3cd034445f4bc351aa00c
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-01-10 17:00:34 +0100

    import vite-plugin-copy(1)

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import copy from 'vite-plugin-copy'
+const copy = require('vite-plugin-copy')
 
 // https://vitejs.dev/config/
 export default defineConfig({
