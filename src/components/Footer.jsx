27a6e1d04c2130fb3f845e20818371e94cb6b744
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-03-15 19:05:44 +0100

    add hidden form for netlify form submission

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,51 +11,71 @@ export const Footer = ({ showOverlay, toggleOverlay }) => {
   const handleNameChange = (event) => {
     setName(event.target.value);
   };
-  
+
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
-  
+
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-};
+  };
 
   return (
-    <footer className="footerHome">
-      <FontAwesomeIcon
-        icon={faEnvelope}
-        className="emailIcon"
-        onClick={toggleOverlay}
-      />
-      {showOverlay && (
-        <div className="overlay">
-          <button onClick={toggleOverlay}>Close</button>
-          <form className="submitForm" onSubmit={handleSubmit} name="contact" data-netlify="true" >
-            <label>
-              Name:
-              <input type="text" name="name" className="formInputTagHome" onChange={handleNameChange} />
-            </label>
-            <label>
-              Message:
-              <textarea
-                type="text"
-                name="message"
-                className="formInputTagHome"
-                onChange={handleMessageChange}
-              />
-            </label>
-            <label>
-              Email:
-              <input type="email" name="email" className="formInputTagHome" onChange={handleEmailChange} />
-            </label>
-            <input type="submit" value="Submit" className="submitBtn" />
-          </form>
-        </div>
-      )}
-    </footer>
+    <>
+      <footer className="footerHome">
+        <FontAwesomeIcon
+          icon={faEnvelope}
+          className="emailIcon"
+          onClick={toggleOverlay}
+        />
+        {showOverlay && (
+          <div className="overlay">
+            <button onClick={toggleOverlay}>Close</button>
+            <form
+              className="submitForm"
+              onSubmit={handleSubmit}
+              name="contact"
+              data-netlify="true"
+            >
+              <label>
+                Name:
+                <input
+                  type="text"
+                  name="name"
+                  className="formInputTagHome"
+                  onChange={handleNameChange}
+                />
+              </label>
+              <label>
+                Message:
+                <textarea
+                  type="text"
+                  name="message"
+                  className="formInputTagHome"
+                  onChange={handleMessageChange}
+                />
+              </label>
+              <label>
+                Email:
+                <input
+                  type="email"
+                  name="email"
+                  className="formInputTagHome"
+                  onChange={handleEmailChange}
+                />
+              </label>
+              <input type="submit" value="Submit" className="submitBtn" />
+            </form>
+          </div>
+        )}
+      </footer>
+      <div style={{ display: "none" }}>
+        <form name="contact" data-netlify="true"></form>
+      </div>
+    </>
   );
 };
