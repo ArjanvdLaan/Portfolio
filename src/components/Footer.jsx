7e9577b2f889909ce00ccc72f935697b0cf77b62
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-03-16 13:40:45 +0100

    remove import URLSearchParams

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import React, { useState, URLSearchParams } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import "./Footer.css";
