2d5206aa902a3272009c5d6ea843518a2ba205c9
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-11-15 14:52:42 +0100

    add build tool configuration for copying the redirects_ file

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import react from '@vitejs/plugin-react'
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+
+    viteStaticCopy({
+      targets: [
+        {
+          src: '_redirects',
+          dest: '' // Root of the dist directory
+        }
+      ]
+    })
+  ],
   server: {
     host: '0.0.0.0',
     proxy: {
