5da0cbff398648e364bd1637088c2259a445fb31
Arjan van der Laan <laanvanderarjan@gmail.com>
2024-12-21 14:09:48 +0100

    add adjustments to title display

--- src/components/RedditClient/components/Title.jsx
+++ src/components/RedditClient/components/Title.jsx
@@ -3,17 +3,22 @@ import { Disclosure } from "@headlessui/react";
 import "./CSS/Title.css";
 
 const Title = ({ post }) => {
-  const [isOverflowing, setIsOverflowing] = useState(false); // State to track if the title is overflowing
   const titleRef = useRef(null); // Ref for the title element
-
 
   // Determine the length of the truncated title
   const titleText = post.data.title;
-  const truncatedLength = 50; // Number of characters to show initially
-  const displayedTitle = titleText.substring(0, truncatedLength);
-  const remainingTitle = titleText.substring(truncatedLength); // Remaining part of the title
+  const truncatedLength = 60; // Number of characters to show initially
 
-  console.log("isOverflowing:", isOverflowing);
+  let displayedTitle = titleText;
+  let remainingTitle = "";
+
+  // Check if the title is overflowing
+  if (titleText.length > truncatedLength) {
+    // Find the last space within the truncated length
+    let endIndex = titleText.lastIndexOf(" ", truncatedLength);
+    displayedTitle = titleText.substring(0, endIndex);
+    remainingTitle = titleText.substring(endIndex).trim(); // Remaining part of the title
+  }
 
   return (
     <div className="disclosure-container">
@@ -29,8 +34,9 @@ const Title = ({ post }) => {
                 overflow: "hidden",
               }}
             >
-              {displayedTitle} 
-              {remainingTitle && '...'} {/* Show ellipsis if there is remaining title */}
+              {displayedTitle}
+              {remainingTitle && "..."}{" "}
+              {/* Show ellipsis if there is remaining title */}
             </Disclosure.Button>
 
             {/* Show down-pointing arrow only if overflowing */}
